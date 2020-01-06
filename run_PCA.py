de124a77c592bcd37f8af95d05cf5ff1b140a767
Elinor Thompson <elinor.thompson@nottingham.ac.uk>
2020-01-06 17:21:45 +0000

    Added comments

--- run_PCA.py
+++ run_PCA.py
@@ -3,9 +3,13 @@ from scipy import sparse
 from numpy import array, matrix
 import sys
 
+#number of principal components to use
+dPCA = 500 
 
-dPCA = 500
+#path to group level connectivity matrix
 cm_path = "/share/neurodev/matrix2/Results/NMF_paper/classifier/average_cm.npz"
+
+#where you want to save the components
 results_dir = "/share/neurodev/matrix2/Results/NMF_paper/classifier/" 
 
 def run_PCA(connectivity_matrix, dPCA) :
@@ -14,7 +18,8 @@ def run_PCA(connectivity_matrix, dPCA) :
 
     pca = PCA(n_components=dPCA)
     PCs = pca.fit_transform(connectivity_matrix)
-    
+
+    #variance explained by all the components, as a percentage of the total    
     explained_variance =  100*np.cumsum(pca.explained_variance_ratio_)[dPCA -1]
 
     return PCs, explained_variance
